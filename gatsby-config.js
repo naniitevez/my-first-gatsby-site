a789a3b8053784131dc7f5e9594903cbccc3d89d
Nicole Tevez <nicole.tevez@fizzmod.com>
2021-11-16 11:51:33 -0300

    "Finished Gatsby Tutorial Part 4"

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My firts gatsby site",
+    title: "Trying gatsby by first",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
